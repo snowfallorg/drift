d65b9027e0173f095064c85f60bc54f70f178177
Jake Hamilton <jake.hamilton@hey.com>
2024-01-26 15:17:14 -0800

    chore: update nixpkgs and add license

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My Nix packages";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     snowfall-lib = {
