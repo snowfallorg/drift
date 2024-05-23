9de8ef5498cbb1ad5851fa87612db87b1637a670
Jake Hamilton <jake.hamilton@hey.com>
2024-05-22 23:03:46 -0700

    chore: pin inputs

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
-  description = "My Nix packages";
+  description = "Update packages with ease.";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     snowfall-lib = {
-      url = "github:snowfallorg/lib";
+      url = "github:snowfallorg/lib?ref=v3.0.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
